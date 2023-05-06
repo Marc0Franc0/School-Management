637da0cfa054d8d0d8db452df6dfc0b4dda9985c
Marc0Franc0 <marcofranco340@gmail.com>
2023-05-05 21:05:07 -0300

    First commit

---
+++ src/main/java/com/api/notemanagementapi/NoteManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.notemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManagementApiApplication.class, args);
+	}
+
+}
