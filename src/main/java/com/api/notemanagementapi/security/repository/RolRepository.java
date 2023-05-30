abdb6dabe2115140b572bf140647f723de487f38
Marc0Franc0 <marcofranco340@gmail.com>
2023-05-30 20:06:24 -0300

    Repositorios de seguridad modificados

--- src/main/java/com/api/notemanagementapi/security/repository/RolRepository.java
+++ src/main/java/com/api/notemanagementapi/security/repository/RolRepository.java
@@ -1,14 +1,10 @@
 package com.api.notemanagementapi.security.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.api.notemanagementapi.security.model.Rol;
+import com.api.notemanagementapi.security.model.RoleEntity;
 
 @Repository
-public interface RolRepository extends JpaRepository<Rol,Long> {
-   //Obtención de un rol por medio de su nombre
-   Optional<Rol>findByName(String name); 
+public interface RolRepository extends JpaRepository<RoleEntity,Long> {
 }
--- src/main/java/com/api/notemanagementapi/security/repository/UserRepository.java
+++ src/main/java/com/api/notemanagementapi/security/repository/UserRepository.java
@@ -3,13 +3,12 @@ package com.api.notemanagementapi.security.repository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.api.notemanagementapi.security.model.User;
+import com.api.notemanagementapi.security.model.UserEntity;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
     //Obtención de un usuario por medio de su nombre de usuario
-    Optional<User>findByUsername(String username);
+    Optional<UserEntity>findByUsername(String username);
     //Verificación de la existencia de un usuario por medio de nombre de usuario
     Boolean existsByUsername(String username);
 }
