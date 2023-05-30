edc1899ed90baaaa2f032e65b80fe618cab5cff9
Marc0Franc0 <marcofranco340@gmail.com>
2023-05-30 20:06:39 -0300

    Delete SecurityConstants.java

--- src/main/java/com/api/notemanagementapi/security/jwt/SecurityConstants.java
+++
@@ -1,6 +0,0 @@
-package com.api.notemanagementapi.security.jwt;
-
-public class SecurityConstants {
-    public static  final long JWT_EXPIRATION_TOKEN = 300000;
-    public static final String JWT_FIRMA = "firma";
-}
